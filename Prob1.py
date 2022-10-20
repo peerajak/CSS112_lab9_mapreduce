3fe1a26371454043d6f48ae424e9cc4b2510c7f9
peerajak <peerajak@gmail.com>
2022-10-20 17:46:54 +0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,10 +1,6 @@
 # Use Map to replace 'The' (case insensitive) with 'xxx'
-def Problem1():
-    atext='The split method is used to split the strings and store them in the list.\
-    The built-in method returns a list of the words in the string, using the “delimiter” as the delimiter string. \
-    If a delimiter is not specified or is None, a different splitting algorithm is applied: \
-    runs of consecutive whitespace are regarded as a single separator, \
-    and the result will contain no empty strings at the start or end if the string has leading or trailing whitespace.'
+def Problem1(atext):
+
     
     ltext = atext.split(" ")
     ####Your code here####
