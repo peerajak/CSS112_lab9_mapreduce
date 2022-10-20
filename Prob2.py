58ab5bd5c2924c9d6987403c76e5fc2a0fcf0338
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-10-20 17:16:06 +0700

    Update and rename Prob2Lagrange.py to Prob2.py

---
+++ Prob2.py
@@ -0,0 +1,20 @@
+#Reduce.  Replace the word 'The', case insensitive, with 'xxx' from a text
+def Lagrange(x,xi,yi):
+def Problem2():
+    atext='The split method is used to split the strings and store them in the list.\
+    The built-in method returns a list of the words in the string, using the “delimiter” as the delimiter string. \
+    If a delimiter is not specified or is None, a different splitting algorithm is applied: \
+    runs of consecutive whitespace are regarded as a single separator, \
+    and the result will contain no empty strings at the start or end if the string has leading or trailing whitespace.'
+    
+    ltext = atext.split(" ")
+
+    ####Your code here####
+    #def relace_the_reduce(
+    #    return ??
+    #####################
+    ####Your code here###
+    #anstext=reduce(
+    #####################
+    return anstext
+
