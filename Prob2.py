d19a44611c34e6d83c7437dfb23132af2e032389
peerajak <peerajak@gmail.com>
2022-10-20 17:47:28 +0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -1,11 +1,9 @@
 #Reduce.  Replace the word 'The', case insensitive, with 'xxx' from a text
+from functools import reduce
 
-def Problem2():
-    atext='The split method is used to split the strings and store them in the list.\
-    The built-in method returns a list of the words in the string, using the “delimiter” as the delimiter string. \
-    If a delimiter is not specified or is None, a different splitting algorithm is applied: \
-    runs of consecutive whitespace are regarded as a single separator, \
-    and the result will contain no empty strings at the start or end if the string has leading or trailing whitespace.'
+
+def Problem2(atext):
+
     
     ltext = atext.split(" ")
 
